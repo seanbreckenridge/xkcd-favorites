e44ab80ac35697fb4bda9d020b6fb69ec3c1e95d
Sean Breckenridge <seanbrecke@gmail.com>
2019-07-28 00:52:27 -0700

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# xkcd-favorites
+
+A github pages website that lists my favorite URLs.
+
+Font used: [ipython/xkcd-font](https://github.com/ipython/xkcd-font)
+
+The MIT License applies to the html/css/python files, the [xkcd](https://xkcd.com/) comics themselves are licensed under [CC BY-NC 2.5](https://creativecommons.org/licenses/by-nc/2.5/)
+
+### Fork
+
+If you wish to create your own favorites list, you can:
+
+1. Fork this repository
+2. `git clone ...` your repository to your computer.
+3. Edit `favorites.yaml` to have your the corresponding IDs for your favorite xkcd's. The IDs are in the URL when viewing an xkcd.
+4. `pip3 install -r requirements.txt`
+5. `python3 generate.py`
+6. `git push`
+
+`generate.py` will use `data.json` as a cache, so any items previously downloaded won't have to be downloaded again.
+
+For debugging whether or not something has already been downloaded, you can increase log verbosity by editing the `LOGLEVEL` envionment variable; e.g.:
+
+`LOGLEVEL=DEBUG python3 generate.py`
+
+More about log levels [here](https://docs.python.org/3.7/howto/logging.html#when-to-use-logging).
---
+++ generate.py
@@ -0,0 +1,80 @@
+import os
+import time
+import json
+import logging
+from io import BytesIO
+
+import yaml
+import requests
+import backoff
+from PIL import Image
+
+root_dir = os.path.dirname(__file__)
+favorites_file = os.path.join(root_dir, 'favorites.yaml')  # source
+data_file = os.path.join(root_dir, 'data.json')  # output
+xkcd_json_api = "http://xkcd.com/{}/info.0.json"
+
+LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
+logging.basicConfig(level=LOGLEVEL, format='%(asctime)s: %(message)s')
+
+def get(url):
+    time.sleep(1)
+    return requests.get(url)
+
+
+@backoff.on_exception(
+    backoff.fibo,
+    requests.exceptions.RequestException,
+    max_tries=20)
+def get_img_data(id):
+    """Get image url and height/width for the image for a xkcd id"""
+    logging.info("Getting metadata for xkcd id: {}".format(id))
+     # get metadata
+    resp = get(xkcd_json_api.format(id)).json()
+    # get img dimensions
+    img_obj = Image.open(BytesIO(get(resp['img']).content))
+    return {"img_url": resp["img"], "dimensions": {"height": img_obj.height, "width": img_obj.width}}
+
+
+def initialize_data():
+    """Read from yaml/json files to get information about new ids/previous runs"""
+
+    # get IDs from yaml file
+    with open(favorites_file, 'r') as f:
+        ids = list(map(str, yaml.load(f, Loader=yaml.FullLoader)))
+
+    # read from data file (i.e. cached values) if it exists
+    json_cache = {}
+    if os.path.exists(data_file):
+        try:
+            with open(data_file, 'r') as f:
+                json_cache = json.load(f)
+        except json.JSONDecodeError:
+            pass
+
+    # if id's from your favorites have been removed, remove them from the data dictionary:
+    for id in list(json_cache):
+        if id not in ids:
+            logging.info("Couldn't find id: '{}' in favorites.yaml, removing from 'data.json'.".format(id))
+            del json_cache[id]
+
+    return ids, json_cache
+
+
+def main():
+    ids, data = initialize_data()
+
+    # if needed, download the information for an id.
+    logging.debug("favorite id list: {}".format(ids))
+    logging.debug("cached ids: {}".format(list(data.keys())))
+    for id in ids:
+        if id not in data:
+            data[id] = get_img_data(id)
+        else:
+            logging.debug("Found information for id '{}' in 'data.json'".format(id))
+
+    with open(data_file, 'w') as f:
+        json.dump(data, f)
+
+if __name__ == "__main__":
+    main()
