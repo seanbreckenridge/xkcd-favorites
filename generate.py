4ce3ca492d947f27cf6ece676fddae437d162652
Sean Breckenridge <seanbrecke@gmail.com>
2020-02-19 09:50:52 -0800

    add type annotations

--- generate.py
+++ generate.py
@@ -3,22 +3,23 @@ import time
 import json
 import logging
 from io import BytesIO
+from typing import Mapping, List, Any
 
 import yaml
 import requests
 import backoff
 from PIL import Image
 
-root_dir = os.path.dirname(__file__)
-favorites_file = os.path.join(root_dir, "favorites.yaml")  # source
-data_file = os.path.join(root_dir, "data.json")  # output
-xkcd_json_api = "http://xkcd.com/{}/info.0.json"
+root_dir: str = os.path.dirname(__file__)
+favorites_file: str = os.path.join(root_dir, "favorites.yaml")  # source
+data_file: str = os.path.join(root_dir, "data.json")  # output
+xkcd_json_api: str = "http://xkcd.com/{}/info.0.json"
 
-LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
+LOGLEVEL: str = os.environ.get("LOGLEVEL", "INFO").upper()
 logging.basicConfig(level=LOGLEVEL, format="%(asctime)s: %(message)s")
 
 
-def get(url):
+def get(url) -> requests.Response:
     time.sleep(1)
     return requests.get(url)
 
@@ -28,9 +29,9 @@ def get_img_data(id):
     """Get image url and height/width for the image for a xkcd id"""
     logging.info("Getting metadata for xkcd id: {}".format(id))
     # get metadata
-    resp = get(xkcd_json_api.format(id)).json()
+    resp: Mapping[str, Any] = get(xkcd_json_api.format(id)).json()
     # get img dimensions
-    img_obj = Image.open(BytesIO(get(resp["img"]).content))
+    img_obj: Image.Image = Image.open(BytesIO(get(resp["img"]).content))
     return {
         "img_url": resp["img"],
         "dimensions": {"height": img_obj.height, "width": img_obj.width},
@@ -42,10 +43,10 @@ def initialize_data():
 
     # get IDs from yaml file
     with open(favorites_file, "r") as f:
-        ids = list(map(str, yaml.load(f, Loader=yaml.FullLoader)))
+        ids: List[str] = list(map(str, yaml.load(f, Loader=yaml.FullLoader)))
 
     # read from data file (i.e. cached values) if it exists
-    json_cache = {}
+    json_cache: Mapping[str, Any] = {}
     if os.path.exists(data_file):
         try:
             with open(data_file, "r") as f:
@@ -68,7 +69,7 @@ def initialize_data():
 
 
 def main():
-    ids, data = initialize_data()
+    ids, data = initialize_data()  # type: ignore
 
     # if needed, download the information for an id.
     logging.debug("favorite id list: {}".format(ids))
